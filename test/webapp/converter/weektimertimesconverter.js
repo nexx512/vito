833c7cd83012f0358c35ab1b70d36028bd34e785
nexx512 <>
2018-09-16 17:00:53 +0200

    Extracted model/dto converter

---
+++ test/webapp/converter/weektimertimesconverter.js
@@ -0,0 +1,59 @@
+should = require("should")
+const WeekTimerTimesConverter = require("../../../webapp/converter/weektimertimesconverter")
+const WeekTimerTimes = require("../../../models/weektimertimes")
+const TimerTimes = require("../../../models/timertimes")
+const TimerTime = require("../../../models/timertime")
+const Time = require("../../../models/time")
+
+describe("A WeekTimerTimes converet", () => {
+
+  it("should transform a WeekTimerTimes model to a WeekTimerTimesDto", () => {
+    let timerTimes = new TimerTimes()
+    timerTimes.add(new TimerTime(new Time("00:01"), new Time("00:0a")))
+    let weekTimerTimesModel = new WeekTimerTimes(timerTimes)
+    weekTimerTimesModel.validate()
+
+    let convertedWeekTimerTimesReponseDto = WeekTimerTimesConverter.toWeekTimerTimesResponseDto(weekTimerTimesModel)
+
+    let weekTimerTimesReponseDto = {
+      monday: new Array(4),
+      tuesday: new Array(4),
+      wednesday: new Array(4),
+      thursday: new Array(4),
+      friday: new Array(4),
+      saturday: new Array(4),
+      sunday: new Array(4)
+    }
+    weekTimerTimesReponseDto.monday[0] = {
+      on: {
+        time: "00:01",
+        errors: []
+      },
+      off: {
+        time: "00:0a",
+        errors: ["Time format invalid"]
+      }
+    }
+    convertedWeekTimerTimesReponseDto.monday.length.should.equal(4)
+    convertedWeekTimerTimesReponseDto.should.eql(weekTimerTimesReponseDto)
+  })
+
+  it("should transform a WeekTimerTimerDto to a WeekTimerTimesModel", () => {
+    let weekTimerTimesReponseDto = {
+      monday: [
+        {on: "00:01", off: "00:0a"},
+        {on: "", off: ""},
+        {on: "", off: ""},
+        {on: "", off: ""}
+      ]
+    }
+
+    let convertedWeekTimerTimesModel = WeekTimerTimesConverter.toWeekTimerTimesModel(weekTimerTimesReponseDto)
+
+    let timerTimes = new TimerTimes()
+    timerTimes.add(new TimerTime(new Time("00:01"), new Time("00:0a")))
+    let weekTimerTimesModel = new WeekTimerTimes(timerTimes)
+    convertedWeekTimerTimesModel.should.eql(weekTimerTimesModel)
+  })
+
+})
--- webapp/controller/warmwater.js
+++ webapp/controller/warmwater.js
@@ -1,11 +1,7 @@
 const WarmWaterService = require("../../services/warmwaterservice")
 const VControlRepo = require("../../repo/vcontrol/vcontrolrepo")
 const VControlClient = require("../../repo/vcontrol/vcontrolclient")
-const WeekTimerTimes = require("../../models/weektimertimes")
-const TimerTimes = require("../../models/timertimes")
-const TimerTime = require("../../models/timertime")
-const Time = require("../../models/time")
-const ViewModel = require("../models/viewmodel.js")
+const WeekTimerTimesConverter = require("../converter/weektimertimesconverter")
 
 module.exports = function(app) {
 
@@ -13,7 +9,7 @@ module.exports = function(app) {
     const warmWaterService = new WarmWaterService(new VControlRepo(new VControlClient()))
     try {
       let heatingTimes = await warmWaterService.getHeatingTimes()
-      res.render("warmwater/heating", new ViewModel(heatingTimes))
+      res.render("warmwater/heating", {model: heatingTimes})
     } catch (e) {
       next(e)
     }
@@ -23,8 +19,8 @@ module.exports = function(app) {
     const warmWaterService = new WarmWaterService(new VControlRepo(new VControlClient()))
     try {
       let circulationTimes = await warmWaterService.getCirculationTimes()
-      let times = weekTimerTimesToWeekTimerTimesDto(circulationTimes)
-      res.render("warmwater/circulation", new ViewModel(times))
+      let times = WeekTimerTimesConverter.toWeekTimerTimesResponseDto(circulationTimes)
+      res.render("warmwater/circulation", {model: times})
     } catch (e) {
       next(e)
     }
@@ -33,50 +29,14 @@ module.exports = function(app) {
   app.put("/warmwater/circulation", async (req, res) => {
     const warmWaterService = new WarmWaterService(new VControlRepo(new VControlClient()))
 
-    let circulationTimes = weekTimerTimesDtoToWeekTimerTimes(req.body.times)
+    let circulationTimes = WeekTimerTimesConverter.toWeekTimerTimesModel(req.body.times)
     try {
       await warmWaterService.setCirculationTimes(circulationTimes)
       res.redirect("/warmwater/circulation")
     } catch (e) {
-      res.render("warmwater/circulation", new ViewModel(weekTimerTimesToWeekTimerTimesDto(circulationTimes), e))
+      res.render("warmwater/circulation", {model: WeekTimerTimesConverter.toWeekTimerTimesResponseDto(circulationTimes), errors: [e.message]})
     }
 
   })
 
-}
-
-function weekTimerTimesDtoToWeekTimerTimes(weekTimerTimesDto) {
-  let weekTimerTimes = new WeekTimerTimes()
-  for (let day in weekTimerTimesDto) {
-    let dayTimes = new TimerTimes()
-    weekTimerTimesDto[day]
-      .filter((time) => {
-        return time.on && time.off
-      })
-      .forEach((time) => {
-        dayTimes.add(new TimerTime(new Time(time.on), new Time(time.off)))
-      })
-    weekTimerTimes.set(day, dayTimes)
-  }
-  return weekTimerTimes
-}
-
-function weekTimerTimesToWeekTimerTimesDto(weekTimerTimes) {
-  let weekTimerTimesDto = {}
-  for (let day in weekTimerTimes.days) {
-    weekTimerTimesDto[day] = new Array(4)
-    weekTimerTimes.days[day].times.forEach((time, index) => {
-      weekTimerTimesDto[day][index] = {
-        on: {
-          time: time.on.time,
-          errors: time.on.errors.items.map((e) => e.message)
-        },
-        off: {
-          time: time.off.time,
-          errors: time.off.errors.items.map((e) => e.message)
-        }
-      }
-    })
-  }
-  return weekTimerTimesDto
 }
