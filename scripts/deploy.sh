0bf77b60210188e1a399d08a68b2cb311d6df8ef
nexx512 <>
2018-10-03 06:30:15 +0200

    Use command line target if available. Ignore configs for deployment.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $DIR/..
 
-USER=pi
-TARGET=vitodens
+TARGET=${1:-pi@vitodens}
 LOG=log/deploy.log
 
 set -e
@@ -19,20 +18,20 @@ echo "OK"
 
 echo -n "Stopping service on target... "
 echo "##### Stopping service on target... " >> $LOG
-ssh $USER@$TARGET "sudo /srv/vito/scripts/local/stop.sh" >> $LOG 2>&1
+ssh $TARGET "sudo /srv/vito/scripts/local/stop.sh" >> $LOG 2>&1
 echo "OK"
 
 echo -n "Copying files to target... "
 echo "##### Copying files to target... " >> $LOG
-rsync -av --del models node_modules repo scripts services webapp app.js $USER@$TARGET:/srv/vito >> $LOG 2>&1
+rsync -av --del models node_modules repo scripts services webapp app.js --exclude config $TARGET:/srv/vito >> $LOG 2>&1
 echo "OK"
 
 echo -n "Rebuilding native modules... "
 echo "##### Rebuilding native modules... " >> $LOG
-ssh $USER@$TARGET "cd /srv/vito; npm rebuild" >> $LOG 2>&1
+ssh $TARGET "cd /srv/vito; npm rebuild" >> $LOG 2>&1
 echo "OK"
 
 echo -n "Starting service on target... "
 echo "##### Starting service on target... " >> $LOG
-ssh $USER@$TARGET "sudo /srv/vito/scripts/local/start.sh app.js" >> $LOG 2>&1
+ssh $TARGET "sudo /srv/vito/scripts/local/start.sh app.js" >> $LOG 2>&1
 echo "OK"
