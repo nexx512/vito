23698c63683c9efac93103079a111b1686cc5640
nexx512 <>
2019-08-20 10:48:55 +0200

    Added copyright and disclaimer to program start

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
 global.Config = require("../config/config.json")
 
+console.log("Vito Copyright (C) 2019 Jürgen Diez\n" +
+"This program comes with ABSOLUTELY NO WARRANTY; for details see LICENCE file. This is free software, and you are welcome to redistribute it under certain conditions; See LICENCE file for details.\n");
+
 require("./webapp/webapp")
